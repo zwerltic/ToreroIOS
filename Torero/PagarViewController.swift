ff00febade6e773e9c1bf160fb16f5db564385ec
Jose <jose@Itzayanas-MacBook-Air.local>
2015-09-18 12:26:47 -0500

    Update with no exceptions

--- Torero/PagarViewController.swift
+++ Torero/PagarViewController.swift
@@ -42,61 +42,78 @@ class PagarViewController: UIViewController {
     @IBAction func tokenizeCard(sender : AnyObject) {
         buttonPagar.setTitle("Cargando . . .", forState: .Normal)
         buttonPagar.enabled = false
-        
+        numberField.resignFirstResponder()
+        nameField.resignFirstResponder()
+        ccField.resignFirstResponder()
+        expMonthField.resignFirstResponder()
+        expYearField.resignFirstResponder()
         var conekta = Conekta(publicKey: "key_EfsX62HbiTSNsuHr5q6xv2Q")
         
-        //var tarjeta = Card(last4: numberField.text, name: nameField.text, cvc: ccField.text, exp_month: expMonthField.text, exp_year: expYearField.text)
-       var tarjeta = Card(last4: "4242424242424242", name: "Julion", cvc: "456", exp_month: "09", exp_year:"2018")
+        var tarjeta = Card(last4: numberField.text, name: nameField.text, cvc: ccField.text, exp_month: expMonthField.text, exp_year: expYearField.text)
+       //var tarjeta = Card(last4: "4242424242424242", name: "Julion", cvc: "456", exp_month: "09", exp_year:"2018")
         var postInfo = nameSaved + lastSaved + streetSaved + numberSaved + coloniaSaved + delegacionSaved + juzgadoSaved + itineranteSaved
         
         conekta.createToken(tarjeta, withSuccess: { (data) -> Void in
             println("Success:")
             println(data)
             var token = data["id"] as! String!
-            let request = NSMutableURLRequest(URL: NSURL(string: "http://torero-tlacuilo.rhcloud.com/api/charge")!)
-            request.HTTPMethod = "POST"
-            
-            let postString = "token=" + token + postInfo
-            println(postString)
-            request.HTTPBody = postString.dataUsingEncoding(NSUTF8StringEncoding)
-            let task = NSURLSession.sharedSession().dataTaskWithRequest(request) {
-                data, response, error in
+            var messageTok = data["message"] as! String!
+            println(token)
+            if token != nil {
+                let request = NSMutableURLRequest(URL: NSURL(string: "http://torero-tlacuilo.rhcloud.com/api/charge")!)
+                request.HTTPMethod = "POST"
                 
-                if error != nil {
-                    println("error=\(error)")
-                    var alert = UIAlertController(title:"Hubo un error", message: "Intentalo de Nuevo", preferredStyle: UIAlertControllerStyle.Alert)
-                    alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default, handler: {(alert: UIAlertAction!) in
-                        self.buttonPagar.setTitle("Pagar", forState: .Normal)
-                        self.buttonPagar.enabled = true
-                        
-                    }))
-                    self.presentViewController(alert, animated: true, completion: nil)
-                    return
-                }
-                
-                println("response = \(response)")
-                
-                let responseString = NSString(data: data, encoding: NSUTF8StringEncoding)
-                println("responseString = \(responseString)")
-                if responseString as! String == "Success" {
-                    var alert = UIAlertController(title:"Pago", message: "Tu pago ha sido exitoso", preferredStyle: UIAlertControllerStyle.Alert)
-                    alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default, handler: {(alert: UIAlertAction!) in
-                    NSOperationQueue.mainQueue().addOperationWithBlock {
-                        self.performSegueWithIdentifier("goToGracias", sender: self)
+                let postString = "token=" + token + postInfo
+                println(postString)
+                request.HTTPBody = postString.dataUsingEncoding(NSUTF8StringEncoding)
+                let task = NSURLSession.sharedSession().dataTaskWithRequest(request) {
+                    data, response, error in
+                    
+                    if error != nil {
+                        println("error=\(error)")
+                        var alert = UIAlertController(title:"Hubo un error", message: "Intentalo de Nuevo", preferredStyle: UIAlertControllerStyle.Alert)
+                        alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default, handler: {(alert: UIAlertAction!) in
+                            self.buttonPagar.setTitle("Pagar", forState: .Normal)
+                            self.buttonPagar.enabled = true
+                            
+                        }))
+                        self.presentViewController(alert, animated: true, completion: nil)
+                        return
                     }
-                   
-                    }))
-                    self.presentViewController(alert, animated: true, completion: nil)
-                } else {
-                    var alert = UIAlertController(title:"Pago", message: "Tu pago ha fallado", preferredStyle: UIAlertControllerStyle.Alert)
-                    alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default, handler: {(alert: UIAlertAction!) in
-                        self.buttonPagar.setTitle("Pagar", forState: .Normal)
-                        self.buttonPagar.enabled = true
-                    }))
-                    self.presentViewController(alert, animated: true, completion: nil)
+                    
+                    println("response = \(response)")
+                    
+                    let responseString = NSString(data: data, encoding: NSUTF8StringEncoding)
+                    println("responseString = \(responseString)")
+                    if responseString as! String == "Success" {
+                        var alert = UIAlertController(title:"Pago", message: "Tu pago ha sido exitoso", preferredStyle: UIAlertControllerStyle.Alert)
+                        alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default, handler: {(alert: UIAlertAction!) in
+                            NSOperationQueue.mainQueue().addOperationWithBlock {
+                                self.performSegueWithIdentifier("goToGracias", sender: self)
+                            }
+                            
+                        }))
+                        self.presentViewController(alert, animated: true, completion: nil)
+                    } else {
+                        var alert = UIAlertController(title:"Pago", message: "La tarjeta no es valida", preferredStyle: UIAlertControllerStyle.Alert)
+                        alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default, handler: {(alert: UIAlertAction!) in
+                            self.buttonPagar.setTitle("Pagar", forState: .Normal)
+                            self.buttonPagar.enabled = true
+                        }))
+                        self.presentViewController(alert, animated: true, completion: nil)
+                    }
                 }
+                task.resume()
+            } else {
+                println("Alert is showing")
+                var alert = UIAlertController(title:"Hubo un error", message: messageTok , preferredStyle: UIAlertControllerStyle.Alert)
+                alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default, handler: {(alert: UIAlertAction!) in
+                    self.buttonPagar.setTitle("Pagar", forState: .Normal)
+                    self.buttonPagar.enabled = true
+                    
+                }))
+                self.presentViewController(alert, animated: true, completion: nil)
             }
-            task.resume()
         }, withFailure: { (error) -> Void in
                 println("Failure:")
                 var alert = UIAlertController(title:"Error", message: "Intentalo de Nuevo", preferredStyle: UIAlertControllerStyle.Alert)
